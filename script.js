72fb4dcc6d64711ca76c223b06d43586a29b8aec
Glitch (obvious-striped-nose) <none>
2021-08-26 23:18:02 +0000

    📮🍅 Checkpoint ./script.js:1024672/4060

--- script.js
+++ script.js
@@ -12,16 +12,25 @@ Parse to
 }
 */
 
-
-function parseRoScript(programString){
+function parseRoScript(programString) {
   alert(programString);
   // Lexor
   var tokens = [];
-  for(var x in programString){
-    
+  var currToken = "";
+  var pos = [1, 1];
+  for (var x in programString) {
+    if (x === " ") {
+      // End of token
+      if (typeof currToken * 1 === "number") {
+        tokens.push(currToken * 1);
+      } else if (currToken !== "") {
+        tokens.push(currToken);
+      }
+    } else {
+      currToken += x;
+    }
   }
+  return tokens;
 }
 
-function runRoScriptAction(actionObject){
-  
-}
+function runRoScriptAction(actionObject) {}
