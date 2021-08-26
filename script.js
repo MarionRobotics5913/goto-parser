9fc92f85bdc35d0dbd376fdd6a85e015668b3961
Glitch (obvious-striped-nose) <none>
2021-08-26 23:42:01 +0000

    🌉🕍 Checkpoint ./script.js:1024672/1440 ./index.html:1024672/280

--- script.js
+++ script.js
@@ -16,24 +16,48 @@ function parseRoScript(programString) {
   // Lexor
   var tokens = [];
   var currToken = "";
-  var pos = [1, 1];
-  for (var x in programString) {
-    var char = programString[x];
-    if (char === " ") {
-      // End of token
+  
+  function appendCurrToken(){
       if (typeof currToken * 1 === "number") {
         tokens.push(currToken * 1);
       } else if (currToken !== "") {
         tokens.push(currToken);
       }
-      currToken = "";
-    } else {
-      currToken += char;
+      currToken = "";    
+  };
+  
+  var x = 0;
+  
+  for (var x in programString) {
+    var char = programString[x];
+    switch(char){
+      case " ":
+        appendCurrToken();
+      break;
+      case "\n":
+        appendCurrToken();
+        tokens.push("newline");        
+        break;
+      
+      default:
+        currToken += char;
     }
+    // if (char === " ") {
+    //   appendToken();
+    // } else if(char === "\n"){
+    //   appendToken();
+    //   tokens.push("newline");
+    // }else {
+    //   currToken += char;
+    // }
   }
   // Add on the last token
   tokens.push(currToken);
   return tokens;
+  
+  
 }
 
-function runRoScriptAction(actionObject) {}
+function runRoScriptAction(actionObject) {
+  
+}
