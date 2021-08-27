d807ab92212782de95eb35c6fdf726d148dcfd05
Glitch (obvious-striped-nose) <none>
2021-08-27 01:45:09 +0000

    🏩🍨 Checkpoint ./script.js:1024672/10263 ./index.html:1024672/107

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ function parseRoScript(programString) {
   //   while (x < programString.length) {
   for (var x in programString) {
     var char = programString[x];
-    if (currToken.endsWith("//")) { // If a comment started
+    if (currToken.endsWith("//")) {
+      // If a comment started
       if (char !== "\n") {
         continue;
       }
@@ -63,13 +64,42 @@ function parseRoScript(programString) {
   // Add on the last token
   tokens.push(currToken);
   // return tokens;
-  
+
   // Parser
   var actions = [];
   var currIndex = 0; // Index instead of object because of possible shallow cloning issues
-  for(var x in tokens){
-    switch()
+  for (var x in tokens) {
+    switch (tokens[x]) {
+      default:
+        return {
+          command: "error",
+          message: `Unexpected token ${x}`
+        };
+    }
+  }
+  return actions;
+}
+
+// This is just for handling stuff on this page and testing
+function runRoScriptActions(actionObject, callback) {
+  for (var x in actionObject) {
+    switch (actionObject[x].command) {
+      case "error":
+        callback(`Error: ${actionObject[x].message}`);
+        return actionObject[x].message;
+        break;
+      case "goto":
+        callback(`Unrecognized command ${actionObject[x].command} run`);
+        break;
+      default:
+        callback(`Unrecognized command ${actionObject[x].command} run`);
+    }
   }
 }
 
-function runRoScriptAction(actionObject) {}
+function runProgram(programString) {
+  document.getElementById("output").innerHTML = "Running...";
+  runRoScriptActions(parseRoScript(programString), output => {
+    document.getElementById("output").innerHTML += output + "<br />";
+  });
+}
