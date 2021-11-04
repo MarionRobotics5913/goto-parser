8015ac9a847d7ae2c0ed09bf06820beb1b7e5852
Glitch (goto-parser) <none>
2021-11-04 22:03:54 +0000

    👵🚒 Checkpoint ./visualeditor.js:36855738/300

--- visualeditor.js
+++ visualeditor.js
@@ -9,8 +9,8 @@ var y = 36;
 var grid_squares = [];
 var gridX = 1;
 var gridY = 1;
-var width = 37,
-  height = 37;
+var width = 5,
+  height = 5;
 var gridLineColor = "#4a4a4a";
 var tan = "#f1ffb8";
 var grey = "#4a4a4a";
@@ -25,10 +25,10 @@ function draw() {
     }
   }
   //the robot
-  ctx.fillStyle = "#c9c9c9";
+  window.fill("#c9c9c9");
   window.rect(x, y, width, height);
   //motors
-  ctx.fillStyle = "#696969";
+  window.fill("#696969");
   //front left
   window.rect(x + width / 16, y + height / 21, width / 3, height / 8);
   //front right
@@ -38,7 +38,7 @@ function draw() {
   //back right
   window.rect(x + width / 1.4, y + height / 1.2, width / 3, height / 8);
   //the treads
-  ctx.fillStyle = "#000000";
+  window.fill("#000000");
   window.rect(
     x - width / 10,
     y - height / 10,
@@ -47,13 +47,13 @@ function draw() {
   );
   window.rect(x + width, y - width / 10, width / 5, height + height / 5);
   //the control bub and the expantion hub
-  ctx.fillStyle = "#4a4a4a";
+  window.fill("#4a4a4a");
   window.rect(x + width / 5, y + height / 2, width / 3, height / 4);
   //battery holder
-  ctx.fillStyle = "#7ce800";
+  window.fill("#7ce800");
   window.rect(x + width / 1.5, y + height / 2.25, width / 5, height / 2.6);
   //battery
-  ctx.fillStyle = "#000000";
+  window.fill("#000000");
   window.rect(x + width / 1.43, y + height / 2.1, width / 8, height / 3.3);
 }
 
