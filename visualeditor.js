fe4975ffc07ee26efe027ea09286c63f6b9f46f2
Glitch (goto-parser) <none>
2021-11-08 22:37:56 +0000

    🚙⚡️ Checkpoint ./visualeditor.js:36855738/1999

--- visualeditor.js
+++ visualeditor.js
@@ -4,13 +4,13 @@ var visualeditor = document.getElementById("visualeditor");
 var VEactivated = false;
 var drawSquares = true;
 
-var xpos = 20;
-var ypos = 20;
+var xpos = 5;
+var ypos = 5;
 var grid_squares = [];
 var gridX = 1;
 var gridY = 1;
-var widthpos = 50,
-  heightpos = 50;
+var widthpos = 37,
+  heightpos = 37;
 var gridLineColor = "#4a4a4a";
 var tan = "#f1ffb8";
 var grey = "#4a4a4a";
@@ -24,63 +24,40 @@ function draw() {
       window.rect(i * 50, j * 50, 50, 50);
     }
   }
-  //the robot
-  //the motors
-  window.fill("#000000");
-    window.rect(
-      xpos - widthpos / 10,
-      ypos - heightpos / 10,
-      widthpos / 5,
-      heightpos + heightpos / 5
-    );
-  window.rect(xpos + widthpos, ypos - widthpos / 10, widthpos / 5, heightpos + heightpos / 5);
-  fillStyle = "#c9c9c9";
-    fillRect(x, y, width, height);
-    //motors
-    fillStyle = "#696969";
-    //front left
-    fillRect(x + width / 16, y + height / 21, width / 3, height / 8);
-    //front right
-    fillRect(x + width / 1.4, y + height / 21, width / 3, height / 8);
-    //back left
-    fillRect(x + width / 16, y + height / 1.2, width / 3, height / 8);
-    //back right
-    fillRect(x + width / 1.4, y + height / 1.2, width / 3, height / 8);
-  //body
   
   
   // the testing
   //the robot
-    fillStyle = "#c9c9c9";
-    fillRect(x, y, width, height);
-    //motors
-    fillStyle = "#696969";
-    //front left
-    fillRect(x + width / 16, y + height / 21, width / 3, height / 8);
-    //front right
-    fillRect(x + width / 1.4, y + height / 21, width / 3, height / 8);
-    //back left
-    fillRect(x + width / 16, y + height / 1.2, width / 3, height / 8);
-    //back right
-    fillRect(x + width / 1.4, y + height / 1.2, width / 3, height / 8);
-    //the treads
-    fillStyle = "#000000";
-    fillRect(
-      x - width / 10,
-      y - height / 10,
-      width / 5,
-      height + height / 5
-    );
-    fillRect(x + width, y - width / 10, width / 5, height + height / 5);
-    //the control bub and the expantion hub
-    fillStyle = "#4a4a4a";
-    fillRect(x + width / 5, y + height / 2, width / 3, height / 4);
-    //battery holder
-    fillStyle = "#7ce800";
-    fillRect(x + width / 1.5, y + height / 2.25, width / 5, height / 2.6);
-    //battery
-    fillStyle = "#000000";
-    fillRect(x + width / 1.43, y + height / 2.1, width / 8, height / 3.3);
+  window.fill("#c9c9c9");
+  window.rect(xpos, ypos, widthpos, heightpos);
+  //motors
+  window.fill("#696969");
+  //front left
+  window.rect(xpos + widthpos / 16, ypos + heightpos / 21, widthpos / 3, heightpos / 8);
+  //front right
+  window.rect(xpos + widthpos / 1.4, ypos + heightpos / 21, widthpos / 3, heightpos / 8);
+  //back left
+  window.rect(xpos + widthpos / 16, ypos + heightpos / 1.2, widthpos / 3, heightpos / 8);
+  //back right
+  window.rect(xpos + widthpos / 1.4, ypos + heightpos / 1.2, widthpos / 3, heightpos / 8);
+  //the treads
+  window.fill("#000000");
+  window.rect(
+    xpos - widthpos / 10,
+    ypos - heightpos / 10,
+    widthpos / 5,
+    heightpos + heightpos / 5
+  );
+  window.rect(xpos + widthpos, ypos - widthpos / 10, widthpos / 5, heightpos + heightpos / 5);
+  //the control bub and the expantion hub
+  window.fill("#4a4a4a");
+  window.rect(xpos + widthpos / 5, ypos + heightpos / 2, widthpos / 3, heightpos / 4);
+  //battery holder
+  window.fill("#7ce800");
+  window.rect(xpos + widthpos / 1.5, ypos + heightpos / 2.25, widthpos / 5, heightpos / 2.6);
+  //batterypos
+  window.fill("#000000");
+  window.rect(xpos + widthpos / 1.43, ypos + heightpos / 2.1, widthpos / 8, heightpos / 3.3);
   
 }
 
