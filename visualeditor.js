97d98a25aba332fbc24b905a470f6e57ec083351
Glitch (goto-parser) <none>
2021-11-10 20:20:11 +0000

    🐩🐃 Checkpoint ./visualeditor.js:1024672/740

--- visualeditor.js
+++ visualeditor.js
@@ -31,91 +31,98 @@ function draw() {
   // the testing
   //the robot
   // var [xpos, ypos, turndegree]
+  // window.translate(xpos + widthpos / 2, ypos + heightpos / 2);
+  window.translate(xpos, ypos );
   window.rotate(Math.PI / 180 * turndegree);
-  window.translate(xpos + widthpos / 2, ypos + heightpos / 2);
+  window.translate(- widthpos / 2, - heightpos / 2)
   window.fill("#c9c9c9");
-  window.rect(xpos, ypos, widthpos, heightpos);
+  window.rect(0, 0, widthpos, heightpos);
   //motors
   window.fill("#696969");
   //front left
   window.rect(
-    /*xpos + widthpos / 16,
-    /*ypos + heightpos / 21,
+    /* xpos + */ widthpos / 16,
+    /* ypos + */ heightpos / 21,
     widthpos / 3,
     heightpos / 8
   );
   //front right
   window.rect(
-    /*xpos + widthpos / 1.4,
-    /*ypos + heightpos / 21,
+    /* xpos + */ widthpos / 1.4,
+    /* ypos + */ heightpos / 21,
     widthpos / 3,
     heightpos / 8
   );
   //back left
   window.rect(
-    /*xpos + widthpos / 16,
-    /*ypos + heightpos / 1.2,
+    /* xpos + */ widthpos / 16,
+    /* ypos + */ heightpos / 1.2,
     widthpos / 3,
     heightpos / 8
   );
   //back right
   window.rect(
-    /*xpos + widthpos / 1.4,
-    /*ypos + heightpos / 1.2,
+    /* xpos + */ widthpos / 1.4,
+    /* ypos + */ heightpos / 1.2,
     widthpos / 3,
     heightpos / 8
   );
   //the treads
   window.fill("#000000");
   window.rect(
-    /*xpos - widthpos / 10,
-    /*ypos - heightpos / 10,
+    /* xpos - */ -widthpos / 10,
+    /* ypos - */ -heightpos / 10,
     widthpos / 5,
     heightpos + heightpos / 5
   );
   window.rect(
-    /*xpos + widthpos,
-    /*ypos - widthpos / 10,
+    /* xpos + */ widthpos,
+    /* ypos - */ -widthpos / 10,
     widthpos / 5,
     heightpos + heightpos / 5
   );
   //the control bub and the expantion hub
   window.fill("#4a4a4a");
   window.rect(
-    /*xpos + widthpos / 5,
-    /*ypos + heightpos / 2,
+    /* xpos + */ widthpos / 5,
+    /* ypos + */ heightpos / 2,
     widthpos / 3,
     heightpos / 4
   );
   //battery holder
   window.fill("#7ce800");
   window.rect(
-    /*xpos + widthpos / 1.5,
-    /*ypos + heightpos / 2.25,
+    /* xpos + */ widthpos / 1.5,
+    /* ypos + */ heightpos / 2.25,
     widthpos / 5,
     heightpos / 2.6
   );
   //batterypos
   window.fill("#000000");
   window.rect(
-    /*xpos + widthpos / 1.43,
-    /*ypos + heightpos / 2.1,
+    /* xpos + */ widthpos / 1.43,
+    /* ypos + */ heightpos / 2.1,
     widthpos / 8,
     heightpos / 3.3
   );
   window.pop();
   
-  window.fill("#f51000");
+  // window.fill("#f51000");
+  window.stroke(255, 0, 0);
+  window.strokeWeight(5);
   //rectangle is supposed to be drawn in the center of the robot
-  window.rect(xpos + (widthpos / 2), ypos + (heightpos / 2),10,10);
+  // window.rect(xpos + (widthpos / 2), ypos + (heightpos / 2),10,10);
+  window.point(xpos, ypos);
   //rectangle is supposed to be drawn at 150,150
-  window.rect(150,150,10,10);
+  // window.rect(150,150,10,10);
+  window.point(150, 150);
+  window.strokeWeight(1);
 }
 document.onkeydown = function(event) {
   switch (event.keyCode) {
     case 37:
       //console.log('left');
-      turndegree+=5;
+      xpos-=5;
       break;
     case 38:
       //console.log('up');
@@ -123,7 +130,7 @@ document.onkeydown = function(event) {
       break;
     case 39:
       //console.log('right');
-      turndegree-=5;
+      xpos+=5;
       break;
     case 40:
       //console.log('down');
@@ -131,11 +138,11 @@ document.onkeydown = function(event) {
       break;
     case 65:
       //console.log('a');
-      xpos-=5;
+      turndegree-=5;
       break;
     case 68:
       //console.log('d');
-      xpos+=5;
+      turndegree+=5;
       break;
   }
   console.log("x: " + xpos + " ypos: "+ ypos);
